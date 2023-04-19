b4ade8043d15d341312e73f9d0fa1ec885f33db2
weverton <wevertonmixalt@gmail.com>
2023-04-19 20:07:45 -0300

    matrizes

--- matrizes.c
+++ matrizes.c
@@ -3,16 +3,57 @@
 int main()
 {
   int linhas, colunas, i, j;
-  printf("Informe a quantidade de linhas e colunas: ");
-  scanf("%d %d", &linhas, &colunas);
-  int matriz[linhas][colunas];
+  printf("Informe a quantidade de linhas da matriz: ");
+  scanf("%d", &linhas);
+  printf("Informe a quantidade de colunas da matriz: ");
+  scanf("%d", &colunas);
+
+  int matriz1[linhas][colunas];
   for (i = 0; i < linhas; i++)
   {
     for (j = 0; j < colunas; j++)
     {
-      printf("Digite o valor da linha %d e coluna %d: ", i + 1, j + 1);
-      scanf("%d", &matriz[i][j]);
+      printf("matriz 1 - Digite o valor da linha %d e coluna %d: ", i + 1, j + 1);
+      scanf("%d", &matriz1[i][j]);
     }
   }
-  printf("A matriz digitada é: \n");
+  printf("Informe a operação que deseja realizar com as duas matrizes (+ ou x): \n");
+  char operacao;
+  scanf(" %c", &operacao);
+  int matriz2[linhas][colunas];
+  for (i = 0; i < linhas; i++)
+  {
+    for (j = 0; j < colunas; j++)
+    {
+      printf("matriz 2 - Digite o valor da linha %d e coluna %d: ", i + 1, j + 1);
+      scanf("%d", &matriz2[i][j]);
+    }
+  }
+  // realiza a operação inserida
+  if (operacao == '+')
+  {
+    for (i = 0; i < linhas; i++)
+    {
+      for (j = 0; j < colunas; j++)
+      {
+        printf("%d ", matriz1[i][j] + matriz2[i][j]);
+      }
+      printf("\n");
+    }
+  }
+  else if (operacao == 'x')
+  {
+    for (i = 0; i < linhas; i++)
+    {
+      for (j = 0; j < colunas; j++)
+      {
+        printf("%d ", matriz1[i][j] * matriz2[i][j]);
+      }
+      printf("\n");
+    }
+  }
+  else
+  {
+    printf("Operação inválida");
+  }
 }
