a238275a86c00a73f64ca563b4104d90932bbfdd
weverton <wevertonmixalt@gmail.com>
2023-05-15 00:43:27 -0300

    questao 2 revisada

--- ex_presenca_ex2.c
+++ ex_presenca_ex2.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
 
-int ackerman(int m, int n)
+int ackermann(int m, int n)
 {
     if (m == 0)
-    {
-        return n+1;
-    }
-    else if (n==0 && m>0)
-    {
-        ackerman(m-1,1);
-    }
-    else if (n>0 &&m>0)
-    {
-        ackerman(m-1, ackerman(m, n-1));
-    }
+        return n + 1;
+    else if (n == 0)
+        return ackermann(m - 1, 1);
+    else
+        return ackermann(m - 1, ackermann(m, n - 1));
 }
 
 int main()
 {
     int m, n;
     scanf("%d %d", &m, &n);
-    printf("%d\n",ackerman(m, n));
+    printf("%d\n", ackermann(m, n));
     return 0;
 }
