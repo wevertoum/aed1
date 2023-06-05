3ac8ec8f1ce467c86622ab4ca18bfcb195e0c65f
weverton <wevertonmixalt@gmail.com>
2023-06-05 11:43:25 -0300

    versao ok do conjunto porem n compila

--- exercicios_avaliativos/conjunto/conjunto_submit.c
+++ exercicios_avaliativos/conjunto/conjunto_submit.c
@@ -1,165 +1,165 @@
-
+#include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-
-#ifndef CONJUNTO_H
-#define CONJUNTO_H
-
-#include <stdbool.h>
-
-#define MAX_ELEMENTOS 100
 
 typedef struct
 {
-  int elementos[MAX_ELEMENTOS];
+  int *elementos;
   int tamanho;
 } Conjunto;
 
-Conjunto *criaConjunto();
-
-void insereElemento(Conjunto *conjunto, int elemento);
-
-void removeElemento(Conjunto *conjunto, int elemento);
-
-bool pertence(Conjunto *conjunto, int elemento);
-
-Conjunto *uniao(Conjunto *conjunto1, Conjunto *conjunto2);
-
-Conjunto *interseccao(Conjunto *conjunto1, Conjunto *conjunto2);
-
-#endif
-
-Conjunto *criaConjunto()
+Conjunto *criaConjunto(int tamanho)
 {
   Conjunto *conjunto = malloc(sizeof(Conjunto));
-  conjunto->tamanho = 0;
+  conjunto->elementos = malloc(tamanho * sizeof(int));
+  conjunto->tamanho = tamanho;
   return conjunto;
 }
 
-void insereElemento(Conjunto *conjunto, int elemento)
+void destroiConjunto(Conjunto *conjunto)
 {
-  if (!pertence(conjunto, elemento))
-  {
-    conjunto->elementos[conjunto->tamanho] = elemento;
-    conjunto->tamanho++;
-  }
+  free(conjunto->elementos);
+  free(conjunto);
 }
 
-void removeElemento(Conjunto *conjunto, int elemento)
+int pertenceConjunto(Conjunto *conjunto, int elemento)
 {
-  int i;
-  for (i = 0; i < conjunto->tamanho; i++)
+  for (int i = 0; i < conjunto->tamanho; i++)
   {
     if (conjunto->elementos[i] == elemento)
     {
-      conjunto->tamanho--;
-      for (; i < conjunto->tamanho; i++)
-      {
-        conjunto->elementos[i] = conjunto->elementos[i + 1];
-      }
-      break;
+      return 1;
     }
   }
+  return 0;
 }
 
-bool pertence(Conjunto *conjunto, int elemento)
+Conjunto *uniaoConjuntos(Conjunto *conjunto1, Conjunto *conjunto2)
 {
-  int i;
-  for (i = 0; i < conjunto->tamanho; i++)
+  int tamanhoUniao = conjunto1->tamanho + conjunto2->tamanho;
+  Conjunto *uniao = criaConjunto(tamanhoUniao);
+
+  int index = 0;
+  for (int i = 0; i < conjunto1->tamanho; i++)
   {
-    if (conjunto->elementos[i] == elemento)
+    uniao->elementos[index] = conjunto1->elementos[i];
+    index++;
+  }
+
+  for (int i = 0; i < conjunto2->tamanho; i++)
+  {
+    if (!pertenceConjunto(conjunto1, conjunto2->elementos[i]))
     {
-      return true;
+      uniao->elementos[index] = conjunto2->elementos[i];
+      index++;
     }
   }
-  return false;
+
+  uniao->tamanho = index;
+
+  return uniao;
 }
 
-Conjunto *uniao(Conjunto *conjunto1, Conjunto *conjunto2)
+Conjunto *intersecaoConjuntos(Conjunto *conjunto1, Conjunto *conjunto2)
 {
-  Conjunto *resultado = criaConjunto();
-  int i;
+  int tamanhoIntersecao = 0;
+  Conjunto *intersecao = criaConjunto(tamanhoIntersecao);
 
-  for (i = 0; i < conjunto1->tamanho; i++)
+  for (int i = 0; i < conjunto1->tamanho; i++)
   {
-    insereElemento(resultado, conjunto1->elementos[i]);
-  }
-
-  for (i = 0; i < conjunto2->tamanho; i++)
-  {
-    insereElemento(resultado, conjunto2->elementos[i]);
-  }
-
-  return resultado;
-}
-
-Conjunto *interseccao(Conjunto *conjunto1, Conjunto *conjunto2)
-{
-  Conjunto *resultado = criaConjunto();
-  int i;
-
-  for (i = 0; i < conjunto1->tamanho; i++)
-  {
-    if (pertence(conjunto2, conjunto1->elementos[i]))
+    if (pertenceConjunto(conjunto2, conjunto1->elementos[i]))
     {
-      insereElemento(resultado, conjunto1->elementos[i]);
+      intersecao->elementos = realloc(intersecao->elementos, (tamanhoIntersecao + 1) * sizeof(int));
+      intersecao->elementos[tamanhoIntersecao] = conjunto1->elementos[i];
+      tamanhoIntersecao++;
     }
   }
 
-  return resultado;
-}
+  intersecao->tamanho = tamanhoIntersecao;
 
-#include <stdio.h>
-
-void imprimeConjunto(Conjunto *conjunto)
-{
-  int i;
-  printf("{ ");
-  for (i = 0; i < conjunto->tamanho; i++)
-  {
-    printf("%d ", conjunto->elementos[i]);
-  }
-  printf("}\n");
+  return intersecao;
 }
 
 int main()
 {
-  Conjunto *conjunto1 = criaConjunto();
-  Conjunto *conjunto2 = criaConjunto();
-  Conjunto *conjuntoUniao;
-  Conjunto *conjuntoInterseccao;
+  int n1, n2;
+  scanf("%d", &n1);
 
-  insereElemento(conjunto1, 1);
-  insereElemento(conjunto1, 2);
-  insereElemento(conjunto1, 3);
-
-  insereElemento(conjunto2, 2);
-  insereElemento(conjunto2, 3);
-  insereElemento(conjunto2, 4);
-
-  printf("Conjunto 1: ");
-  imprimeConjunto(conjunto1);
-
-  printf("Conjunto 2: ");
-  imprimeConjunto(conjunto2);
-
-  int elemento = 2;
-  if (pertence(conjunto1, elemento))
+  Conjunto *conjunto1 = criaConjunto(n1);
+  for (int i = 0; i < n1; i++)
   {
-    printf("O elemento %d pertence ao conjunto 1\n", elemento);
-  }
-  else
-  {
-    printf("O elemento %d não pertence ao conjunto 1\n", elemento);
+    scanf("%d", &conjunto1->elementos[i]);
   }
 
-  conjuntoUniao = uniao(conjunto1, conjunto2);
-  printf("União dos conjuntos: ");
-  imprimeConjunto(conjuntoUniao);
+  scanf("%d", &n2);
 
-  conjuntoInterseccao = interseccao(conjunto1, conjunto2);
-  printf("Intersecção dos conjuntos: ");
-  imprimeConjunto(conjuntoInterseccao);
+  Conjunto *conjunto2 = criaConjunto(n2);
+  for (int i = 0; i < n2; i++)
+  {
+    scanf("%d", &conjunto2->elementos[i]);
+  }
+
+  int t;
+  scanf("%d", &t);
+
+  for (int i = 0; i < t; i++)
+  {
+    int operacao, elemento;
+    scanf("%d", &operacao);
+
+    switch (operacao)
+    {
+    case 1:
+      scanf("%d", &elemento);
+      if (pertenceConjunto(conjunto1, elemento))
+      {
+        printf("S\n");
+      }
+      else
+      {
+        printf("N\n");
+      }
+      break;
+
+    case 2:
+      scanf("%d", &elemento);
+      if (pertenceConjunto(conjunto2, elemento))
+      {
+        printf("S\n");
+      }
+      else
+      {
+        printf("N\n");
+      }
+      break;
+
+    case 3:
+    {
+      Conjunto *uniao = uniaoConjuntos(conjunto1, conjunto2);
+      for (int j = 0; j < uniao->tamanho; j++)
+      {
+        printf("%d ", uniao->elementos[j]);
+      }
+      printf("\n");
+      destroiConjunto(uniao);
+    }
+    break;
+
+    case 4:
+    {
+      Conjunto *intersecao = intersecaoConjuntos(conjunto1, conjunto2);
+      for (int j = 0; j < intersecao->tamanho; j++)
+      {
+        printf("%d ", intersecao->elementos[j]);
+      }
+      printf("\n");
+      destroiConjunto(intersecao);
+    }
+    break;
+    }
+  }
+
+  destroiConjunto(conjunto1);
+  destroiConjunto(conjunto2);
 
   return 0;
 }
